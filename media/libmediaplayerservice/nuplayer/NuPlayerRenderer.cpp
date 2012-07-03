2bd48c1d60701b861fe908dba852e7dcd7f164af
ApurupaPattapu <apurupa@codeaurora.org>
2012-07-03 13:49:36 -0700

    nuplayer: Fixes in nuplayer Renderer
    - Fix in AV sync calculation for video only stream's pause
      scenario
    
    Change-Id: Ie4fae760ca485ee21121cf4018a62d5d86eacb57

--- media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -1262,6 +1262,9 @@ void NuPlayer::Renderer::onPause() {
         mAudioSink->pause();
         startAudioOffloadPauseTimeout();
     }
+    //for video only stream, reset mAnchorTimeMediaUs on stream's pause scenario
+    if (mHasVideo && !mHasAudio)
+        mAnchorTimeMediaUs = -1;
 
     ALOGV("now paused audio queue has %d entries, video has %d entries",
           mAudioQueue.size(), mVideoQueue.size());
