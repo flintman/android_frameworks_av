108f0f149037c300604030e9623a0d9f8cb252c7
Guoliang Ji <guoliang.ji@intel.com>
2013-05-02 16:43:52 +0800

    accelerate camera recording start up speed
    When do initial buffer submit, if do drainInputBuffers() first, it will always wait all input buffers reached,
    and then start fillOutputBuffers().During this time, nothing to do in level codec, all time is cost on waiting,
    it is especially severe for Camera recording case, for example, in 30fps case, If 3 input buffers are required,
    HW codec will wait at least 99ms to start video encoding, since no outport buffer ready at this
    time.
    
    After change call sequence, fillOutputBuffers() is done fast firstly, then do drainInputBuffers(),
    when receive one input buffer in HW codec, it can start working immediately, because the working condition is reached,
    both input port and out port buffers are ready.
    
    Change-Id: If74fd839f2bb0cae6c7d8ae0912d47abb2cdc045
    Signed-off-by: jiguoliang <guoliang.ji@intel.com>
    Signed-off-by: ywan171 <yi.a.wang@intel.com>

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -4211,13 +4211,13 @@ status_t OMXCodec::read(
             mPaused = false;
         }
 
-        drainInputBuffers();
 
         if (mState == EXECUTING) {
             // Otherwise mState == RECONFIGURING and this code will trigger
             // after the output port is reenabled.
             fillOutputBuffers();
         }
+        drainInputBuffers();
     }
 
     if (seeking) {
