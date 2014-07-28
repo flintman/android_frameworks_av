47955eef8de181f887f5b24adb13d3361d3d6a7f
Manikanta Kanamarlapudi <kmanikan@codeaurora.org>
2014-07-28 18:38:46 +0530

    libstagefright: camcorder pause-resume implementation
    - Add pause resume feature in camcorder app. So that
      user can pause recording and resume later which results
      in a single recorded clip.
    
    Change-Id: Id19c45ae5bb85265aa4d5304b160ebf119d9575a

--- media/libstagefright/MediaCodecSource.cpp
+++ media/libstagefright/MediaCodecSource.cpp
@@ -603,6 +603,9 @@ status_t MediaCodecSource::onStart(MetaData *params) {
             resume();
         } else {
             CHECK(mPuller != NULL);
+            if (mIsVideo) {
+                mEncoder->requestIDRFrame();
+            }
             mPuller->resume();
         }
         return OK;
@@ -815,7 +818,7 @@ void MediaCodecSource::onMessageReceived(const sp<AMessage> &msg) {
     }
     case kWhatPause:
     {
-        if (mFlags && FLAG_USE_SURFACE_INPUT) {
+        if (mFlags & FLAG_USE_SURFACE_INPUT) {
             suspend();
         } else {
             CHECK(mPuller != NULL);
