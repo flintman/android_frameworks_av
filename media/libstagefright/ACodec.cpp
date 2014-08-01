58c8399b15ea67cd82ce05f62ae9c408664a6326
Praveen Chavan <pchavan@codeaurora.org>
2014-08-01 01:52:40 -0700

    stagefright: Expose decoder extradata via native MediaCodec
    Stash GraphicBuffer in filled output so the clients of
    native MediaCodec (during surface-mode playback) can retrieve
    buffer information; not limited to dimensions, color-
    format, decoded YUV data (including trailing extradata)...
    
    User Extradata can be requested by setting
    "enable-extradata-user" in the configuration format.
    
    Change-Id: Iaa49f448a9692e1c1dfe037ea71ddeef73d14d66

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -4675,6 +4675,7 @@ bool ACodec::BaseState::onOMXFillBufferDone(
 
                 mCodec->mPortEOS[kPortIndexOutput] = true;
             }
+            info->mData->meta()->setObject("graphic-buffer", info->mGraphicBuffer);
             break;
         }
 
