cb20799652a260633397c7b68504f87003277b34
Steve Kondik <steve@cyngn.com>
2015-01-18 14:02:40 -0800

    stagefright: More bit depth plumbing
     * Plumb the bit depth thru more parts of Stagefright.
    
    Change-Id: I0f2cfc17d7b9b6e8cf2e26237fc8d0ab261857cd

--- media/libstagefright/FFMPEGSoftCodec.cpp
+++ media/libstagefright/FFMPEGSoftCodec.cpp
@@ -341,8 +341,6 @@ status_t FFMPEGSoftCodec::setAudioFormat(
     ALOGV("setAudioFormat called");
     status_t err = OK;
 
-    ALOGV("setAudioFormat: %s", msg->debugString(0).c_str());
-
     if (isEncoder) {
         ALOGE("Encoding not supported");
         err = BAD_VALUE;
@@ -393,6 +391,8 @@ status_t FFMPEGSoftCodec::setAudioFormat(
             ALOGE("setFFmpegAudioFormat() failed (err = %d)", err);
         }
     }
+
+    ALOGV("setAudioFormat: %s", msg->debugString(0).c_str());
 
     return err;
 }
@@ -577,6 +577,75 @@ status_t FFMPEGSoftCodec::setFFmpegVideoFormat(
 }
 
 //audio
+status_t FFMPEGSoftCodec::setRawAudioFormat(
+        const sp<AMessage> &msg, sp<IOMX> OMXhandle, IOMX::node_id nodeID)
+{
+    int32_t numChannels = 0;
+    int32_t sampleRate = 0;
+    int32_t bitsPerSample = 0;
+
+    CHECK(msg->findInt32(ExtendedCodec::getMsgKey(kKeyChannelCount), &numChannels));
+    CHECK(msg->findInt32(ExtendedCodec::getMsgKey(kKeySampleRate), &sampleRate));
+    if (!msg->findInt32(ExtendedCodec::getMsgKey(kKeyBitsPerSample), &bitsPerSample)) {
+        ALOGD("No PCM format specified, let decoder decide");
+    }
+
+    OMX_PARAM_PORTDEFINITIONTYPE def;
+    InitOMXParams(&def);
+    def.nPortIndex = kPortIndexOutput;
+
+    status_t err = OMXhandle->getParameter(
+            nodeID, OMX_IndexParamPortDefinition, &def, sizeof(def));
+
+    if (err != OK) {
+        return err;
+    }
+
+    def.format.audio.eEncoding = OMX_AUDIO_CodingPCM;
+
+    err = OMXhandle->setParameter(
+            nodeID, OMX_IndexParamPortDefinition, &def, sizeof(def));
+
+    if (err != OK) {
+        return err;
+    }
+
+    OMX_AUDIO_PARAM_PCMMODETYPE pcmParams;
+    InitOMXParams(&pcmParams);
+    pcmParams.nPortIndex = kPortIndexOutput;
+
+    err = OMXhandle->getParameter(
+            nodeID, OMX_IndexParamAudioPcm, &pcmParams, sizeof(pcmParams));
+
+    if (err != OK) {
+        return err;
+    }
+
+    pcmParams.nChannels = numChannels;
+    pcmParams.eNumData = OMX_NumericalDataSigned;
+    pcmParams.bInterleaved = OMX_TRUE;
+    if (bitsPerSample > 0) {
+        pcmParams.nBitPerSample = bitsPerSample;
+    }
+    pcmParams.nSamplingRate = sampleRate;
+    pcmParams.ePCMMode = OMX_AUDIO_PCMModeLinear;
+
+    if (getOMXChannelMapping(numChannels, pcmParams.eChannelMapping) != OK) {
+        return OMX_ErrorNone;
+    }
+
+    err = OMXhandle->setParameter(
+            nodeID, OMX_IndexParamAudioPcm, &pcmParams, sizeof(pcmParams));
+
+    if (err != OK) {
+        return err;
+    }
+
+    msg->setInt32("bits-per-sample", pcmParams.nBitPerSample);
+
+    return OK;
+}
+
 status_t FFMPEGSoftCodec::setWMAFormat(
         const sp<AMessage> &msg, sp<IOMX> OMXhandle, IOMX::node_id nodeID)
 {
