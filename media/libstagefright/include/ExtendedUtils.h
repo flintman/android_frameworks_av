Jackeagle/caf_frameworks_av

libstagefright: Get rtp port range and cache size from system properties
- Operators require setting rtp/rtcp network port range and cache size.
- Parse rtp network port range from persistent system property
  "persist.sys.media.rtp-ports".
- Get cache size from persistent system property
  "persist.sys.media.cache-params", in order to store the value.

Change-Id: Ie044682c422d71f018e509a89a19d976f313e27c

media/libstagefright/include/ExtendedUtils.h
@@ -156,6 +156,9 @@ struct ExtendedUtils {
     static void createSecurePool();
 
     static void drainSecurePool();
+
+    //helper function to parse rtp port range form system property
+    static void parseRtpPortRangeFromSystemProperty(unsigned *start, unsigned *end);
 };
 
 class VSyncLocker : public RefBase {

flintman/android_frameworks_av

media/libstagefright/include/ExtendedUtils.h
@@ -66,6 +66,9 @@ struct ExtendedUtils {
         static bool isSmoothStreamingEnabled();
 
         static int64_t getMaxAVSyncLateMargin();
+
+        //helper function to parse rtp port range form system property
+        static void getRtpPortRange(unsigned *start, unsigned *end);
     };
 
     static const int32_t kNumBFramesPerPFrame = 1;